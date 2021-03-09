80ac99af0f2a834d70b1019ce8a1f45dd599e0da
Kai Winter <kaiwinter@gmx.de>
2021-03-09 22:40:05 +0100

    Removed unused import

--- app/src/main/java/com/github/kaiwinter/nfcsonos/storage/SharedPreferencesTokenStore.java
+++ app/src/main/java/com/github/kaiwinter/nfcsonos/storage/SharedPreferencesTokenStore.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import java.util.Optional;
-
 public class SharedPreferencesTokenStore {
 
     private static final String KEY_REFRESH_TOKEN = "KEY_REFRESH_TOKEN";
