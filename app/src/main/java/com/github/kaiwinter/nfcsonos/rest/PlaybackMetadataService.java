5f2f5faf8d65718051f3b9036849da43e743a0b0
Kai Winter <kaiwinter@gmx.de>
2021-06-14 21:23:32 +0200

    Javadoc

--- app/src/main/java/com/github/kaiwinter/nfcsonos/rest/PlaybackMetadataService.java
+++ app/src/main/java/com/github/kaiwinter/nfcsonos/rest/PlaybackMetadataService.java
@@ -6,9 +6,18 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
+/**
+ * The playbackMetadata namespace includes commands and events that enable your app to receive
+ * notification on changes to metadata for the currently playing track and the next track.
+ */
 public interface PlaybackMetadataService {
 
+    /**
+     * Use this command to poll for metadata changes.
+     *
+     * @param group groupId to determine the target of the command
+     * @return Returns a {@link PlaybackMetadata} object for the target group.
+     */
     @GET("/control/api/v1/groups/{group}/playbackMetadata")
     Call<PlaybackMetadata> loadPlaybackMetadata(@Path("group") String group);
-
 }
