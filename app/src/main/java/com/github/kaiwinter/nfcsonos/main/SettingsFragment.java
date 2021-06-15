6c6d21305e858c005cf17a5af3f1334a0043612d
Kai Winter <kaiwinter@gmx.de>
2021-06-15 21:54:23 +0200

    Allow to change the household/group

--- app/src/main/java/com/github/kaiwinter/nfcsonos/main/SettingsFragment.java
+++ app/src/main/java/com/github/kaiwinter/nfcsonos/main/SettingsFragment.java
@@ -8,9 +8,8 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.github.kaiwinter.nfcsonos.R;
-import com.github.kaiwinter.nfcsonos.databinding.FragmentPairBinding;
 import com.github.kaiwinter.nfcsonos.databinding.FragmentSettingsBinding;
+import com.github.kaiwinter.nfcsonos.discover.DiscoverActivity;
 import com.github.kaiwinter.nfcsonos.login.LoginActivity;
 import com.github.kaiwinter.nfcsonos.storage.SharedPreferencesStore;
 
@@ -28,6 +27,7 @@ public class SettingsFragment extends Fragment {
         sharedPreferencesStore = new SharedPreferencesStore(getActivity().getApplicationContext());
 
         binding = FragmentSettingsBinding.inflate(getLayoutInflater());
+        binding.discoverButton.setOnClickListener(__ -> startDiscoverActivity());
         binding.logoutButton.setOnClickListener(__ -> signOut());
     }
 
@@ -36,13 +36,17 @@ public class SettingsFragment extends Fragment {
         return binding.getRoot();
     }
 
-    public void signOut() {
+    private void startDiscoverActivity() {
+        Intent intent = new Intent(getActivity(), DiscoverActivity.class);
+        startActivity(intent);
+    }
+
+    private void signOut() {
         sharedPreferencesStore.setTokens(null, null, -1);
         sharedPreferencesStore.setHouseholdAndGroup(null, null);
 
         Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
         loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(loginIntent);
-        getActivity().finish();
     }
 }
