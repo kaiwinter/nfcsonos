5ab450dd2fe85be54a69761737670a4118071b9b
Kai Winter <kaiwinter@gmx.de>
2021-07-05 22:33:34 +0200

    Added third party notices

--- app/src/main/java/com/github/kaiwinter/nfcsonos/main/SettingsFragment.java
+++ app/src/main/java/com/github/kaiwinter/nfcsonos/main/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.github.kaiwinter.nfcsonos.main;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,10 +9,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.kaiwinter.nfcsonos.R;
 import com.github.kaiwinter.nfcsonos.databinding.FragmentSettingsBinding;
 import com.github.kaiwinter.nfcsonos.discover.DiscoverActivity;
 import com.github.kaiwinter.nfcsonos.login.LoginActivity;
 import com.github.kaiwinter.nfcsonos.storage.SharedPreferencesStore;
+
+import de.psdev.licensesdialog.LicenseResolver;
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.License;
 
 /**
  * The settings fragment.
@@ -28,6 +34,7 @@ public class SettingsFragment extends Fragment {
 
         binding = FragmentSettingsBinding.inflate(getLayoutInflater());
         binding.discoverButton.setOnClickListener(__ -> startDiscoverActivity());
+        binding.thirdPartyNotices.setOnClickListener(__ -> startLicensesDialog());
         binding.logoutButton.setOnClickListener(__ -> signOut());
     }
 
@@ -41,6 +48,15 @@ public class SettingsFragment extends Fragment {
         startActivity(intent);
     }
 
+    private void startLicensesDialog() {
+        LicenseResolver.registerLicense(createLicense("mixkit", R.raw.mixkit_license_summary, ""));
+        LicenseResolver.registerLicense(createLicense("freepik", -1, "http://www.freepik.com"));
+        LicenseResolver.registerLicense(createLicense("flaticon", -1, "https://www.flaticon.com/authors/flat-icons"));
+        new LicensesDialog.Builder(getActivity())
+                .setNotices(R.raw.notices)
+                .build().show();
+    }
+
     private void signOut() {
         sharedPreferencesStore.setTokens(null, null, -1);
         sharedPreferencesStore.setHouseholdAndGroup(null, null);
@@ -48,5 +64,37 @@ public class SettingsFragment extends Fragment {
         Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
         loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(loginIntent);
+    }
+
+    public static License createLicense(String name, int summaryFile, String summaryText) {
+        return new License() {
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public String readSummaryTextFromResources(Context context) {
+                if (summaryFile == -1) {
+                    return summaryText;
+                }
+                return getContent(context, summaryFile);
+            }
+
+            @Override
+            public String readFullTextFromResources(Context context) {
+                return "";
+            }
+
+            @Override
+            public String getVersion() {
+                return "";
+            }
+
+            @Override
+            public String getUrl() {
+                return "";
+            }
+        };
     }
 }
